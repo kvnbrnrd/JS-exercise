cd5e1488e351bc1af22ff72339b206cedcf8c8da
kvnbrnrd <kevos.directos@gmail.com>
2018-10-17 15:29:46 +0200

    début ex2

--- test.js
+++ test.js
@@ -15,38 +15,34 @@
 //     var myP = document.getElementById("map");
 //          if (myP.style.fontSize === "medium")
 //     {
-        
+
 //         myP.style.fontSize = "large";
 //     }
 //     }
 
 
 //      if (myP.style.fontSize === "large")
- 
+
 
 
 var myP = document.getElementById("map");
 myP.style.fontSize = "medium";
 
-function zoom() 
-{
-    
+function zoom() {
 
 
 
-    if (myP.style.fontSize === "medium")
-    {
+
+    if (myP.style.fontSize === "medium") {
         myP.style.fontSize = "large";
     }
-    else if (myP.style.fontSize === "large")
-    {
+    else if (myP.style.fontSize === "large") {
         myP.style.fontSize = "medium";
     }
-    else
-    {}
+    else { }
 }
 console.log(myP.style.fontSize);
-document.getElementById("button").addEventListener("click", zoom );
+document.getElementById("button").addEventListener("click", zoom);
 
 
 
