3fee95158113a334f097cd9766fef88ecbd9874a
kvnbrnrd <kevos.directos@gmail.com>
2018-10-17 11:29:28 +0200

    exercice1

---
+++ test.js
@@ -0,0 +1,20 @@
+
+ 
+function zoomin(){
+        var myImg = document.getElementById("map");
+        var currWidth = myImg.clientWidth;
+        if(currWidth == 2500) return false;
+         else{
+            myImg.style.width = (currWidth + 100) + "px";
+        } 
+    }
+    function zoomout(){
+        var myImg = document.getElementById("map");
+        console.log(myImg);
+        var currWidth = myImg.clientWidth;
+        if(currWidth == 100) return false;
+		 else{
+            myImg.style.width = (currWidth - 100) + "px";
+           
+        }
+    }
